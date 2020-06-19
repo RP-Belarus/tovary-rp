91f900344f03d86b3c2f026c71a79895bbf6ff56
Dmitry Derevitsky <d_der@mail.ru>
2020-06-19 13:30:22 +0300

    Updates

---
+++ precache-manifest.fe7dcdeba5c230f38b1c5da9fdc1124f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09b457eb354c138ea31a33c9ce7678f",
+    "url": "/tovary-rp/index.html"
+  },
+  {
+    "revision": "ad5f83804f3ada45a46e",
+    "url": "/tovary-rp/static/css/main.f43ee28a.chunk.css"
+  },
+  {
+    "revision": "c6ce7ecbe117c15f8a5c",
+    "url": "/tovary-rp/static/js/2.f63fe087.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tovary-rp/static/js/2.f63fe087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5f83804f3ada45a46e",
+    "url": "/tovary-rp/static/js/main.fcd5bcdf.chunk.js"
+  },
+  {
+    "revision": "9ae581aa30b94caa2efa",
+    "url": "/tovary-rp/static/js/runtime-main.3de7e9e8.js"
+  }
+]);
